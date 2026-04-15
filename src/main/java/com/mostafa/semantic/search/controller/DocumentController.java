fdc1fedd029a4b4e3c4c2113724dd66a5734a6bf
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-04-15 23:58:55 +0200

    update PostgreSQL port in configuration and add DocumentController for document management

---
+++ src/main/java/com/mostafa/semantic/search/controller/DocumentController.java
@@ -0,0 +1,42 @@
+package com.mostafa.semantic.search.controller;
+
+import java.util.List;
+
+import org.springframework.ai.document.Document;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mostafa.semantic.search.dto.AddDocumentRequest;
+import com.mostafa.semantic.search.service.DocumentService;
+
+import jakarta.validation.Valid;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@Controller
+@RequestMapping("/api/documents")
+public class DocumentController {
+
+    private final DocumentService documentService;
+
+    public DocumentController(DocumentService documentService) {
+        this.documentService = documentService;
+    }
+
+    @PostMapping
+    public String add(@RequestBody @Valid AddDocumentRequest request) {
+        documentService.addDocument(request.content());
+        return "Document added successfully";
+    }
+    
+
+    @GetMapping("/search")
+    public List<Document> search( @RequestParam String search){
+        return documentService.searchDocuments(search);
+    }
+
+}
