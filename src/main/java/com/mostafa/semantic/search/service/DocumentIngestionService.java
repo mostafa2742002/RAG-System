8fa478289b680262eafeecedd791ddd0bb082325
mostafa2742002 <mostafa19500mahmoud@gmail.com>
2026-04-16 13:43:58 +0200

    add: implement DocumentIngestionService for text and PDF file ingestion

---
+++ src/main/java/com/mostafa/semantic/search/service/DocumentIngestionService.java
@@ -0,0 +1,65 @@
+package com.mostafa.semantic.search.service;
+
+import java.util.List;
+
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
+import org.springframework.ai.reader.pdf.config.PdfDocumentReaderConfig;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.VectorStore;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocumentIngestionService {
+
+
+    private final VectorStore vectorStore;
+
+    public DocumentIngestionService(VectorStore vectorStore) {
+        this.vectorStore = vectorStore;
+    }
+
+    public int ingestTextFile(String classPathLocation){
+        TextReader textReader =  new TextReader( new ClassPathResource(classPathLocation) );
+        List<Document> rawDocuments = textReader.get();
+
+        List<Document> splitDocuments = TokenTextSplitter.builder()
+        .withChunkSize(800)
+        .withMinChunkSizeChars(350)
+        .withMinChunkLengthToEmbed(10)
+        .withMaxNumChunks(1000)
+        .withKeepSeparator(true)
+        .build()
+        .apply(rawDocuments);
+
+        vectorStore.add(splitDocuments);
+
+        return splitDocuments.size();
+
+    }
+
+     public int ingestPdfFile(String classpathLocation) {
+        PagePdfDocumentReader pdfReader = new PagePdfDocumentReader(
+                new ClassPathResource(classpathLocation),
+                PdfDocumentReaderConfig.builder()
+                        .withPagesPerDocument(1)
+                        .build()
+        );
+
+        List<Document> rawDocuments = pdfReader.read();
+
+        List<Document> splitDocuments = TokenTextSplitter.builder()
+                .withChunkSize(800)
+                .withMinChunkSizeChars(350)
+                .withMinChunkLengthToEmbed(10)
+                .withMaxNumChunks(2000)
+                .withKeepSeparator(true)
+                .build()
+                .apply(rawDocuments);
+
+        vectorStore.add(splitDocuments);
+        return splitDocuments.size();
+    }
+}
